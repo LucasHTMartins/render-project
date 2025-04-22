2b46015048b9cb91c7d3e2ef54967bb0c73ebc35
Lucas H. T. Martins <lucas.henrique.martins@hotmail.com>
2025-04-22 15:07:42 -0600

    uibuild

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+require("dotenv").config();
+
 const express = require("express");
-const app = express();
 const morgan = require("morgan");
 const Person = require("./models/person");
 
+const app = express();
 app.use(express.json());
 app.use(express.static("dist"));
 
@@ -12,68 +14,58 @@ morgan.token("body", (req) => {
 
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"));
 
-
-const PORT = process.env.PORT || 3001;
-
-let data = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+const PORT = process.env.PORT;
 
 app.get("/", (req, res) => {
-    res.send("<div>go to '/api/persons' for data</div>");
+    res.send("<div>go to '/api/persons' for data or '/info'</div>");
 });
 
-app.get("/api/persons", (req, res) => {
+app.get("/api/persons", (req, res, next) => {
     Person
         .find({})
-        .then(persons => {res.json(persons)});
+        .then(persons => {res.json(persons)})
+        .catch(error => next(error));
 });
 
-app.get("/api/persons/:id", (req, res) => {
+app.get("/api/persons/:id", (req, res, next) => {
     const id = req.params.id;
-    const note = data.find(p => p.id === id);
-    if (!note) {
-        res.status(404).end();
-    } else {
-        res.json(note);
-    }
+    Person.findById(id)
+        .then(person => {
+            if (person) {
+                res.json(person);
+            } else {
+                res.status(404).end();
+            }
+        })
+        .catch(error => next(error))
 });
 
-app.delete("/api/persons/:id", (req, res) => {
+app.delete("/api/persons/:id", (req, res, next) => {
     const id = req.params.id;
-    data = data.filter(p => p.id !== id)
-    console.log("deleting: ", id);
-    res.status(204).end();
+    Person.findByIdAndDelete(id)
+        .then(() => {
+            res.status(204).end()
+        })
+        .catch(error => next(error));
 });
 
 app.get("/info", (req, res) => {
-    const now = new Date().toLocaleString();
-    res.send(
-        `<div>Phonebook has info for ${data.length} people</div>
-        <div>${now}<div>`
-    )
+  const now = new Date().toLocaleString();
+  Person.countDocuments({})
+    .then(count => {
+      res.send(
+        `<div>Phonebook has info for ${count} people</div>
+         <div>${now}</div>`
+      );
+    })
+    .catch(error => {
+      console.error(error);
+      res.status(500).send("Error fetching info");
+    });
 });
 
-app.post("/api/persons", (req, res) => {
+
+app.post("/api/persons", (req, res, next) => {
     const p = req.body;
 
     if (!p.name) {
@@ -84,23 +76,48 @@ app.post("/api/persons", (req, res) => {
         return res.status(400).json({
             error: "number missing"
         })
-    } else if (data.some(person => person.name.toLowerCase() === p.name.toLowerCase())) {
-        return res.status(400).json({
-            error: "name already exists"
-        })
     }
 
     const id = Math.floor(Math.random() * 10000);
 
-    const np = {
+    const np = new Person({
         name:p.name,
         number:p.number,
         id: id,
-    };
+    });
 
-    data = data.concat(np);
-    res.json(np);
+    np.save()
+        .then(savedPerson => res.json(savedPerson))
+        .catch(error => next(error));
 });
+
+app.put("/api/persons/:id", (req, res, next) => {
+    const { name, number } = req.body;
+
+    if (!name || !number) {
+        return res.status(400).json({ error: "name or number missing" });
+    }
+
+    Person.findByIdAndUpdate(req.params.id, {name, number}, {new:true})
+    .then(updatedPerson => {
+        if (updatedPerson) {
+            res.json(updatedPerson);
+        } else {
+            res.status(404).end();
+        }
+    })
+    .catch(error => next(error));
+});
+
+
+const unknownEndpoint = (req, res) => {
+    res.status(404).send({error: "unknown endpoint"})
+};
+
+app.use(unknownEndpoint);
+
+const errorHandler = require("./middleware/errorHandler");
+app.use(errorHandler);
 
 app.listen(PORT, () => {
     console.log("server running on port", PORT);
